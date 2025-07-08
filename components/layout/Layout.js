694593f601b6f2437f63e46f298c802d2ed4d8ef
parsa dokhtmohammadi <parsadokhtmohammadi.work@gmail.com>
2025-07-08 15:21:53 +0330

    Layout.js completed

---
+++ components/layout/Layout.js
@@ -0,0 +1,21 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Layout = ({children}) => {
+  return (
+    <>
+        <header className='header'>
+            <h2>MyCRM</h2>
+            <Link href={"/add-customer"}>add customer</Link>
+        </header>
+        <div className='main'>
+            {children}
+        </div>
+        <footer className='footer'>
+            <a href='https://github.com/ParsaDokhtMohammadi/CRM-app-nextJs-mongoDB' target='_blank' rel='noreferrer' >PDM</a> nextJs CRM project &copy;
+        </footer>
+    </>
+  )
+}
+
+export default Layout
