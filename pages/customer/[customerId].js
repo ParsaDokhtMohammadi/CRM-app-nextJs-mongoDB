80ccbddd3b82ebb5fff941faccb52f650811a424
parsa dokhtmohammadi <parsadokhtmohammadi.work@gmail.com>
2025-07-10 11:22:40 +0330

    customer details page created

--- pages/customer/[customerId].js
+++ pages/customer/[customerId].js
@@ -1,14 +1,22 @@
+import CustomerDetailsPage from '@/components/template/CustomerDetailsPage'
 import { useRouter } from 'next/router'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const index = () => {
-    const [data , setData] = useState(null)
+    const [data, setData] = useState(null)
     const router = useRouter()
-    const {query:{customerId},isReady} = router
-    
-  return (
-    <div>index</div>
-  )
+    const { query: { customerId }, isReady } = router
+    useEffect(() => {
+        if (isReady) {
+            fetch(`/api/customer/${customerId}`)
+                .then(res => res.json())
+                .then(data => setData(data.data))
+        }
+    }, [isReady])
+
+    if (data) return (
+        <CustomerDetailsPage data={data}></CustomerDetailsPage>
+    )
 }
 
 export default index
